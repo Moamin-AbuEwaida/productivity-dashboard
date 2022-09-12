be15f50d910e053cf81cd3a98608dd0df64d3123
Moamin <moamin.salamah@gmail.com>
2022-09-12 11:07:20 +0200

    setting up the project

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-green-300 font-bold">Hello world</h1>
+</template>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/base.css';
+
+
+import './assets/base.css'
+
+createApp(App).mount('#app')
